e28981a51fe7060334f50b142a355865e9203b13
HansumaeCecilia <48033@edu.raseko.fi>
2023-01-23 10:28:39 +0200

    Nimilista vaihtoehto 2

--- Harjoitus1.py
+++ Harjoitus1.py
@@ -1,8 +1,20 @@
+# Parit pikanäppäimet:
+#V SCoden komentopaletti         Ctrl+Shift+P
+# Rivien kommentointi            Ctrl + ' (Windows)
+
+# Koodi, joka kysyy viisi nimeä ja tulostaa ne listaan nimeltä "nimet"
+
 nimet = []
 
 
-for i in [1, 6]:
-    nimi = input('Anna nimi: ' + str(i) + ': ')
+#for i in [1, 6]:
+    #nimi = input('Anna nimi: ' + str(i) + ': ')
+    #nimet.append(nimi)
+
+while True:
+    nimi = input('Anna nimi: ')
     nimet.append(nimi)
+    if len(nimet) >= 5:
+        break
 
 print(nimet)

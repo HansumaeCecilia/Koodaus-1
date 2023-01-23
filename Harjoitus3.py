486bf6ae1a1f9aec100b0d88cc0cca0858c24065
HansumaeCecilia <48033@edu.raseko.fi>
2023-01-23 13:29:29 +0200

    pprint, time, etc

--- Harjoitus3.py
+++ Harjoitus3.py
@@ -1,4 +1,6 @@
 import time
+import pprint
+from time import ctime
 
 aikaleima = time.time()
 print(aikaleima)
@@ -14,3 +16,15 @@ aika2 = time.time()
 print(aika1)
 print(aika2)
 print(aika2 - aika1)
+
+esimerkki_kuvio = {
+    'nimi': 'Esimerkki',
+    'tyyppi': 'monikulmio',
+    'pisteet': [
+        {'x': 10, 'y': 20, 'z': 30},
+        {'x': 20, 'y': 5, 'z': 10},
+        {'x': 30, 'y': 15, 'z': 20},
+    ]
+}
+
+pprint.pprint(esimerkki_kuvio, sort_dicts=False)

4b247becfb25d673327ea1bb77b3e4f799974324
HansumaeCecilia <48033@edu.raseko.fi>
2023-01-23 10:36:00 +0200

    4 vaihtoehtoa nimilistalle

--- Harjoitus1.py
+++ Harjoitus1.py
@@ -6,13 +6,37 @@
 
 nimet = []
 
+#Vaihtoehto 1
+
+#nimi = input('Anna nimi: ' + str(i) + ': ')
+#nimet.append(nimi)
+#nimi = input('Anna nimi: ' + str(i) + ': ')
+#nimet.append(nimi)
+#nimi = input('Anna nimi: ' + str(i) + ': ')
+#nimet.append(nimi)
+#nimi = input('Anna nimi: ' + str(i) + ': ')
+#nimet.append(nimi)
+#nimi = input('Anna nimi: ' + str(i) + ': ')
+#nimet.append(nimi)
+
+# Vaihtoehto 2
 
 #for i in [1, 6]:
     #nimi = input('Anna nimi: ' + str(i) + ': ')
     #nimet.append(nimi)
 
+# Vaihtoehto 3
+
+#while True:
+    #nimi = input('Anna nimi: ')
+    #nimet.append(nimi)   
+    #if len(nimet) < 5:
+        #break
+
+# Vaihtoehto 4
+
 while len(nimet) < 5:
     nimi = input('Anna nimi: ')
     nimet.append(nimi)        
 
-print(nimet)
+print('Nimet: ', nimet)

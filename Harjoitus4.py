45ebc958bc9aabc59d74279b42232dee204c6c5d
HansumaeCecilia <48033@edu.raseko.fi>
2023-01-23 14:21:14 +0200

    class Ihminen

---
+++ Harjoitus4.py
@@ -0,0 +1,23 @@
+import datetime
+
+class Ihminen:
+    def __init__(self, etunimi, sukunimi, syntymavuosi): # Aina parametri self alkuun, kun _init_ (MUISTA TUPLA-ALAVIIVAT!)
+        self.etunimi = etunimi
+        self.sukunimi = sukunimi
+        self.syntymavuosi = syntymavuosi
+    
+    def __str__(self):
+        return self.etunimi + " " + self.sukunimi + " (" + str(self.syntymavuosi) + ")"
+
+    def laske_ika(self):
+        nykyinen_vuosi = datetime.date.today().year
+        return nykyinen_vuosi - self.syntymavuosi
+
+henkilo1 = Ihminen("Pentti", "Peikkonen", 1999)
+henkilo2 = Ihminen("Raija", "Ratanen", 1995)
+
+print(henkilo1)
+print(henkilo2)
+
+print(henkilo1.sukunimi, henkilo1.etunimi)
+print(henkilo2.sukunimi, henkilo2.etunimi)

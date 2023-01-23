026869509ff692833543276b09d22eeb9c66a874
HansumaeCecilia <48033@edu.raseko.fi>
2023-01-23 13:46:20 +0200

    Opelta kopsattu, pprint, time, etc.

--- Harjoitus3.py
+++ Harjoitus3.py
@@ -1,30 +1,24 @@
-import time
-import pprint
+from pprint import pformat, pprint
 from time import ctime
+import time
 
 aikaleima = time.time()
-print(aikaleima)
-
-aikateksti = time.ctime(aikaleima)
-print(aikateksti)
-
-aika1 = time.time()
-time.sleep(3)
-
-aika2 = time.time()
-
-print(aika1)
-print(aika2)
-print(aika2 - aika1)
 
 esimerkki_kuvio = {
-    'nimi': 'Esimerkki',
-    'tyyppi': 'monikulmio',
-    'pisteet': [
-        {'x': 10, 'y': 20, 'z': 30},
-        {'x': 20, 'y': 5, 'z': 10},
-        {'x': 30, 'y': 15, 'z': 20},
+    "nimi": "Esimerkki",
+    "tyyppi": "monikulmio",
+    "luotu": ctime(aikaleima),
+    "pisteet": [
+        {"x": 10, "y": 20, "z": 30},
+        {"x": 20, "y": 5, "z": 10},
+        {"x": 30, "y": 15, "z": 20},
     ]
 }
 
-pprint.pprint(esimerkki_kuvio, sort_dicts=False)
+pprint(esimerkki_kuvio, sort_dicts=False)
+
+esimerkki_kuvio_muotoiltu_teksti = pformat(esimerkki_kuvio)
+
+# Muutetaan muotoiltu teksti isoiksi kirjaimiksi ja tulostetaan
+# Huom: \n  =  rivinvaihto
+print("Muotoiltu teksti:\n", esimerkki_kuvio_muotoiltu_teksti.upper())

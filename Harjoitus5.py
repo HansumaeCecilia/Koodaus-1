00e5e45e22d12b43d30bfbb90dad3cf9edb303d0
HansumaeCecilia <48033@edu.raseko.fi>
2023-01-30 10:18:27 +0200

    auto class

---
+++ Harjoitus5.py
@@ -0,0 +1,18 @@
+class Car:
+    def __init__ (self, make, model, year, registration, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.registration = registration
+        self.color = color
+
+    def __str__(self):
+        return self.make + " " + self.model + " (" + str(self.year) + ") " + self. registration + " " + self.color + " "
+
+car1 = Car("Mercedes Benz", "CLA,", 2015, "ABC-123,", "black")
+car2 = Car("Audi", "A6", 2004, "BCD-234,", "blue")
+car3 = Car("Opel", "Corsa", 2010, "CDE-345,", "red")
+
+print(car1)
+print(car2)
+print(car3)
